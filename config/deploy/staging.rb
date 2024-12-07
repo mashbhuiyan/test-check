1ea6f121c33e030a1d72710b332848733e5fc77b
mashbhuiyan <mannan.bhuiyann@gmail.com>
2024-12-07 23:47:48 +0600

    updated the workflows

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,15 +3,15 @@ set :nvm_node, 'v17.9.1'
 # set :deploy_to, '/home/ec2-user/apps/client_api_staging/'
 set :deploy_user, 'ubuntu'
 # set :deploy_to, '/home/mannan/apps/client_api_staging/'
-set :deploy_to, '/home/ubuntu/apps/'
+set :deploy_to, '/home/ubuntu/apps//client_api/'
 set :branch, 'staging'
 
 # role :app, %w{ec2-user@54.243.152.229 ec2-user@34.224.81.91}
-role :app, %w{ubuntu@18.189.30.188}
+role :app, %w{ubuntu@3.139.94.143}
 
 # server '54.243.152.229', user: 'ec2-user', roles: %w{web app}, my_property: :my_value
 # server '34.224.81.91', user: 'ec2-user', roles: %w{web app}, my_property: :my_value
-server '18.189.30.188', user: 'ubuntu', roles: %w{web app}, my_property: :my_value
+server '3.139.94.143', user: 'ubuntu', roles: %w{web app}, my_property: :my_value
 
 
 namespace :deploy do
